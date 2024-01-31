e7fd6c1e4cdb45e07d56bc2e2137d476456af1de
Jostein Skaar <jostein.skaar@gmail.com>
2024-01-31 22:44:44 +0100

    feat: version 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,52 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import logo from '$lib/assets/logo.png';
+</script>
+
+<h1>Lock the Loo</h1>
+<img src={logo} alt="Lock the Loo logo is of a smiling door giving thumbs up" />
+<p>
+  When visiting a loo, we need to make sure the door is locked. Get all the training needed to be a responsible and aware employee in a
+  workplace environment.
+</p>
+
+<a href="/game">Get started</a>
+
+<style>
+  p {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    max-width: 60ch;
+  }
+
+  img {
+    max-width: min(400px, 100%);
+    width: auto;
+    height: auto;
+  }
+
+  a {
+    display: inline-block;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    color: white;
+    background-color: #007bff;
+    border: none;
+    border-radius: 5px;
+    font-size: 16px;
+    cursor: pointer;
+    transition:
+      background-color 0.3s,
+      box-shadow 0.3s;
+  }
+
+  a:hover {
+    background-color: #0056b3;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  }
+
+  a:active {
+    background-color: #004085;
+    box-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.3);
+  }
+</style>
---
+++ src/routes/game/+page.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+  import lockOpen from '$lib/assets/lock-open.png';
+  import lockLocked from '$lib/assets/lock-locked.png';
+
+  let level = 1;
+  let locks = Array.from({ length: level * level }, () => false);
+  let showNextLevelButton = false;
+
+  function toggleLock(index: number) {
+    locks[index] = !locks[index];
+
+    if (locks.every((lock) => lock)) {
+      showNextLevelButton = true;
+    }
+  }
+
+  function increaseLevel() {
+    showNextLevelButton = false;
+    level++;
+    locks = Array.from({ length: level * level }, () => false);
+  }
+
+  function getFinishTextForLevel(level: number) {
+    switch (level) {
+      case 1:
+        return "Good job! Now let's try something harder!";
+      case 2:
+        return 'Wow! 4 locks. Getting the hang of it now!';
+      case 3:
+        return "You're a pro! Can you get to level 5?";
+      case 4:
+        return '🎶You. Are. Amazing! 🎶';
+      case 5:
+        return 'You should be good to go to work now. If not sure, keep going!';
+      case 6:
+        return "That's the spirit!";
+      case 7:
+        return "That's a lot of locks!";
+      default:
+        return 'This is insane! Keep going!';
+    }
+  }
+</script>
+
+<h1>Let's lock 'em locks</h1>
+
+<h2>Level {level}</h2>
+
+<div class="grid-container" style="--level: {level};">
+  {#each locks as isLocked, index}
+    <div class="grid-item">
+      <button on:click={() => toggleLock(index)}>
+        <img src={isLocked ? lockLocked : lockOpen} alt="Lock" />
+      </button>
+    </div>
+  {/each}
+</div>
+
+{#if showNextLevelButton}
+  <p>{getFinishTextForLevel(level)}</p>
+  <button class="next-level-button" on:click={increaseLevel}> Level {level + 1}</button>
+{/if}
+
+<style>
+  .grid-container {
+    display: grid;
+    gap: 10px;
+    grid-template-columns: repeat(var(--level), 1fr);
+    grid-template-rows: repeat(var(--level), 1fr);
+  }
+
+  @media (min-width: 600px) {
+    .grid-container {
+      max-width: 600px;
+    }
+  }
+
+  .grid-item {
+    text-align: center;
+  }
+  button {
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  button img {
+    max-width: min(160px, 100%);
+    width: auto;
+    height: auto;
+  }
+
+  button.next-level-button {
+    display: inline-block;
+    padding: 10px 20px;
+    text-align: center;
+    color: white;
+    background-color: #007bff;
+    border: none;
+    border-radius: 5px;
+    font-size: 16px;
+    cursor: pointer;
+    transition:
+      background-color 0.3s,
+      box-shadow 0.3s;
+  }
+
+  button.next-level-button:hover {
+    background-color: #0056b3;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  }
+
+  button.next-level-button:active {
+    background-color: #004085;
+    box-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.3);
+  }
+
+  p {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+</style>
